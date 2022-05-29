cbffb8d9c195394276373b1726ef152afefb3483
unknown <lk000055668@gmail.com>
2022-05-29 21:59:28 +0800

    add page

---
+++ index.js
@@ -0,0 +1,75 @@
+var firebase;
+$(function(){
+  var $name = $('#name'),
+      $content = $('#content'),
+      $btn = $('#btn'),
+      $show = $('#show'),
+      ms = new Date().getTime();
+  var config = {
+    databaseURL: "https://webduino-chat-363a7-default-rtdb.firebaseio.com/"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database().ref();
+  
+  $btn.on('click',write);
+  $content.on('keydown', function(e){
+    if(e.keyCode == 13){
+      write();
+    }
+  });
+  
+  function write(){
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    if(h<10){
+      h = '0'+h;
+    }
+    if(m<10){
+      m = '0' + m;
+    }
+    if(s<10){
+      s = '0' + s;
+    }
+    var now = h+':'+m+':'+s;
+    var postData = {
+      name:$('#name').val(),
+      content:$('#content').val(),
+      time:now,
+      id:'id'+ms
+    };
+    database.push(postData);
+    $content.val('');
+  }
+  
+  database.once('value', function(snapshot) {
+    $show.html('');
+    for(var i in snapshot.val()){
+       $show.append('<div><div class="time">'+snapshot.val()[i].time+'</div><div class="name">'+snapshot.val()[i].name+' 說</div><div class="content">'+snapshot.val()[i].content+'</div>');
+    }
+    $show.scrollTop($show[0].scrollHeight);
+  });
+
+  database.limitToLast(1).on('value', function(snapshot) {
+    for(var i in snapshot.val()){
+       $show.append('<div class="'+snapshot.val()[i].id+'"><div class="time">'+snapshot.val()[i].time+'</div><div class="name">'+snapshot.val()[i].name+' 說</div><div class="content">'+snapshot.val()[i].content+'</div>');
+    }
+    $show.scrollTop($show[0].scrollHeight);
+    $show.find('.id'+ms+' .name').css({
+      'float':'right',
+      'padding-top':'12px',
+      'color':'#000080'
+    });
+    $show.find('.id'+ms+' .content').css({
+      'float':'right',
+      'margin-right':'10px'
+    });
+    $show.find('.id'+ms+' .time').css({
+      'right':'0',
+      'color':'#777'
+    });
+  });
+  
+  
+});
